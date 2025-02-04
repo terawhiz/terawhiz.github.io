3f2be86fe0423d57ed6cb7b0eb81a2e3d3529534
terawhiz <terawhiz@gmail.com>
2025-02-04 21:22:56 +0530

    add: idk sorry future shunt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://terawhiz.github.io/',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    })
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), mdx(), react()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'ayu-dark',
+        dark: 'kanagawa-dragon'
+      }
+    }
+  },
   trailingSlash: 'ignore',
   server: {
     port: 4321,
-    host: true,
+    host: "0.0.0.0",
   },
   devToolbar: {
     enabled: false,
